73f95a3ed33cc5a3f4bda8931d31bf67df67b38d
[Enrique Marquez] <riquemarquez802@gmail.com>
2025-10-30 13:46:56 -0300

    Update prod

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -11,11 +11,6 @@ declare class Debugger {
     private handleEvent;
 }
 
-declare const isEnvBrowser: () => boolean;
-declare const noop: () => void;
-
-declare const useObserve: <T = unknown>(action: string, handler: (data: T) => void) => void;
-
 declare class Post<T = unknown> {
     private eventName;
     private data?;
@@ -25,12 +20,38 @@ declare class Post<T = unknown> {
     private execute;
 }
 
+declare const isEnvBrowser: () => boolean;
+
+declare const lazyLoad: <T extends Record<string, any>, U extends keyof T>(loader: (x?: string) => Promise<T>) => T;
+
+declare const sleep: (delay: number) => Promise<unknown>;
+
+declare const noop: () => void;
+
+declare const useObserve: <T = unknown>(action: string, handler: (data: T) => void) => void;
+
 type EventHandlerSignature<T = Event> = (event: T) => void;
 declare const useListen: <T extends Event = Event>(event: string, handler: EventHandlerSignature<T>, target?: EventTarget) => void;
+
+declare const useImageValidation: (imageUrl: string) => {
+    isImageValid: boolean;
+    imageUrl: string;
+};
+
+interface UseSoundOptionsInterface {
+    volume?: number;
+    loop?: boolean;
+}
+
+declare const useSound: (src: string, options?: UseSoundOptionsInterface) => {
+    play: () => void;
+    pause: () => void;
+    isPlaying: boolean;
+};
 
 type ObservedMessageType<T = any> = {
     action: string;
     data: T;
 };
 
-export { Debugger, type DebuggerEventType, type ObservedMessageType, Post, isEnvBrowser, noop, useListen, useObserve };
+export { Debugger, type DebuggerEventType, type ObservedMessageType, Post, isEnvBrowser, lazyLoad, noop, sleep, useImageValidation, useListen, useObserve, useSound };
